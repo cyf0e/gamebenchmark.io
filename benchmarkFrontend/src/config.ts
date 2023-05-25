b5e4e43d01b400e906bd7d63595b99ccb08391e3
cyf0e <cyf001@protonmail.com>
2023-05-25 10:57:44 +0200

    helper variable

--- benchmarkFrontend/src/config.ts
+++ benchmarkFrontend/src/config.ts
@@ -1,5 +1,6 @@
-//change url to backend url
-export const BASE_API_URL = "http://localhost:8080";
+//change url to the public url of your backend
+const PUBLIC_API_URL = undefined;
+export const BASE_API_URL = PUBLIC_API_URL || "http://localhost:8080";
 const DEFAULT_API_KEY = "enter_your_api_key_here";
 export let API_KEY = localStorage.getItem("API_KEY") || DEFAULT_API_KEY;
 export function setApiKey(key: string) {
