3a60eb7cb0bd888a3facd1c6de4b1527399ef5a0
cyf0e <cyf001@protonmail.com>
2023-06-03 21:37:18 +0200

    Added env variable for api

--- benchmarkFrontend/src/config.ts
+++ benchmarkFrontend/src/config.ts
@@ -1,5 +1,6 @@
-//change url to the public url of your backend
-const PUBLIC_API_URL = undefined;
+//change url to the public url of your backend or read env variable
+//@ts-ignore
+const PUBLIC_API_URL = import.meta.env.VITE_PUBLIC_URL;
 export const BASE_API_URL = PUBLIC_API_URL || "http://localhost:8080";
 const DEFAULT_API_KEY = "enter_your_api_key_here";
 export let API_KEY = localStorage.getItem("API_KEY") || DEFAULT_API_KEY;
