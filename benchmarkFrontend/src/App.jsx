dae23ea158e0699b4bfb3cb942a5486a5d3ffc74
cyf0e <cyf001@protonmail.com>
2023-06-06 09:51:47 +0200

    added initial loading state

--- benchmarkFrontend/src/App.jsx
+++ benchmarkFrontend/src/App.jsx
@@ -1,12 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Loader } from "./components/Loader";
 import "./index.css";
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/games");
+  }, []);
   return (
     <>
       <div className="App">
-        <NavBar />
-        <Outlet />
+        <Loader>
+          <NavBar />
+          <Outlet />
+        </Loader>
       </div>
     </>
   );
--- benchmarkFrontend/src/components/NavBar.tsx
+++ benchmarkFrontend/src/components/NavBar.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 export const NavBar = () => {
   const navigate = useNavigate();
-  useEffect(() => {
-    navigate("/games");
-  }, []);
+
   return (
     <div className="navbar-container">
       <h1 style={{ margin: 0, display: "inline-block" }}>GameBenchmark.io</h1>
